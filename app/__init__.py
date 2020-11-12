9f3d13b7b5e4559126c4768dba5f8371237a6033
mark <markmcclatchy@gmail.com>
2020-11-12 15:33:51 -0500

    create a form to collect data to ship a package - minus redirect

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 from .config import Configuration
+from .shipping_form import ShippingForm
 
 app = Flask(__name__)
-
 app.config.from_object(Configuration)
+
 
 
 @app.route("/")
@@ -13,4 +14,5 @@ def index():
 
 @app.route("/new_package", methods=["GET", "POST"])
 def new_package():
-    return render_template("shipping_request.html")
+    form = ShippingForm()
+    return render_template("shipping_request.html", form=form)
